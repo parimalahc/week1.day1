53945b8f78451081e1d4be1876aa48047e1baa4f
parimalahc <42540345+parimalahc@users.noreply.github.com>
2022-01-24 17:28:58 +0530

    Update Mobile.java

--- Mobile.java
+++ Mobile.java
@@ -2,18 +2,28 @@ package week1.day1;
 
 public class Mobile {
 	
-	public void sendMsg() {}
-	
-	private void makeCall() {}
-	
-	void saveContact() {}
-
 	public static void main(String[] args) {
 		Mobile obj1 = new Mobile();
 		obj1.sendMsg();
-		obj1.makeCall();
-		obj1.saveContact();
+		obj1.makeCall(9876789878L);
+		obj1.saveContact("John", 7788998877L);
 
+	}
+
+	public void sendMsg() {
+		String name = "Daisy";
+		String msg = "Hi How are you?";
+		System.out.println(name);
+		System.out.println(msg);
+	}
+
+	private void makeCall(long num) {
+		System.out.println("Call to number " + num);
+	}
+
+	void saveContact(String name, long num) {
+		System.out.println(name);
+		System.out.println(num);
 	}
 
 }
